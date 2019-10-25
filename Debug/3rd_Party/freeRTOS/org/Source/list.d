3403a5ccdee30e74511656a3bf35058cf97ed647
ajay-fupro <ajay.rajan@fupro.in>
2019-10-25 20:02:31 +0530

    First commit

---
+++ Debug/3rd_Party/freeRTOS/org/Source/list.d
@@ -0,0 +1,26 @@
+3rd_Party/freeRTOS/org/Source/list.o: \
+ ../3rd_Party/freeRTOS/org/Source/list.c \
+ Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/include/FreeRTOS.h \
+ Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/include/FreeRTOSConfig.h \
+ Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/include/projdefs.h \
+ Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/include/portable.h \
+ Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/include/deprecated_definitions.h \
+ Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/include/mpu_wrappers.h \
+ Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/include/list.h
+
+Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/include/FreeRTOS.h:
+
+Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/include/FreeRTOSConfig.h:
+
+Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/include/projdefs.h:
+
+Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/include/portable.h:
+
+Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/include/deprecated_definitions.h:
+
+Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/include/mpu_wrappers.h:
+
+Z:/RTOS_Workspace/STM32_Blinky/3rd_Party/freeRTOS/org/Source/include/list.h:
